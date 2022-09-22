39b3d930c85148905f1778bd4d18dd577bcb69b2
Maxim Kucheriaviy <maxim-k@i.ua>
2022-09-22 12:58:10 +0300

    some edits

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import axios from "axios";
-//import { ImageGalleryItem } from "components/ImageGalleryItem/ImageGalleryItem";
+import { ImageGalleryItem } from "components/ImageGalleryItem/ImageGalleryItem";
+import { TailSpin } from "react-loader-spinner";
 
 
 export class ImageGallery extends Component{
@@ -10,12 +11,16 @@ export class ImageGallery extends Component{
         super(props);
         this.state = {
             page: 1,
-            renderImages: []
+            renderImages: [],
+            isLoading: false
         }
     }
     async componentDidUpdate(prevProps, prevState){
         if(this.props.keyword && (prevProps.keyword !== this.props.keyword || prevState.page !== this.state.page)){
             try{
+                this.setState({
+                    isLoading: true
+                })
                 const response = await axios.get(this.url, 
                 {
                     params:{
@@ -25,17 +30,47 @@ export class ImageGallery extends Component{
                         per_page: 12
                     }
                 })
-                console.log(response);
+                const images = response.data.hits.map(item => {
+                    return {
+                        id: item.id,
+                        webUrl: item.webformatURL,
+                        largeUrl: item.largeImageURL
+                    }
+                })
+                this.setState({
+                    renderImages: images
+                })
             }
             catch(err){
                 console.log(err);
+            }
+            finally{
+                this.setState({
+                    isLoading: false
+                })
             }
         }
     }
     render(){
         return(
             <ul className="ImageGallery">
-                {this.props.keyword && `Cards and ${this.props.keyword}`}
+                {this.state.isLoading && <TailSpin
+                    height="80"
+                    width="80"
+                    color="#4fa94d"
+                    ariaLabel="tail-spin-loading"
+                    radius="1"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                    visible={true}
+                />}
+                {this.props.keyword && 
+                this.state.renderImages.map(item => 
+                    <ImageGalleryItem 
+                        src={item.webUrl} 
+                        alt={this.state.keyword}
+                        key={item.id}
+                    />)}
             </ul>
         )
     }
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -2,10 +2,10 @@ import { Component } from "react";
 
 export class ImageGalleryItem extends Component{
     render(){
-        const {src, alt, key} = this.props;
+        const {src, alt} = this.props;
         return(
             <li className="gallery-item">
-                <img src={src} alt={alt} key={key}/>
+                <img src={src} alt={alt}/>
             </li>
         )
     }
