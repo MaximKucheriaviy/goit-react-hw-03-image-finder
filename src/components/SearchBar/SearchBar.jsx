bccc44ec84e85167eb2cdda82002fb2183bd68d5
Maxim Kucheriaviy <maxim-k@i.ua>
2022-09-24 00:18:20 +0300

    searchbarFix

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,6 @@
 import { Component } from "react";
 import PropTypes from "prop-types";
 import { AiOutlineSearch } from "react-icons/ai";
-import { Loader } from "components/Loder/Loader";
-
-
 
 export class SearchBar extends Component{
     state = {
